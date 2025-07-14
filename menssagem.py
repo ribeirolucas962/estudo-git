49fc7ae0cac6c1d7ceeeb1908be35e44450ddd05
Lucas Ribeiro <ribeirolucas962@gmail.com>
2025-07-14 20:18:06 -0300

    adição de soma de dois números.py

--- menssagem.py
+++ menssagem.py
@@ -1,2 +1,12 @@
 messagem =  "hello world! Utilizando o Git junto o vscosde!"
 print(messagem)
+
+def soma( a, b ):
+    return a + b
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = soma(num1, num2)
+
+print(f"A soma de {num1} e {num2} é: {resultado}")
+
