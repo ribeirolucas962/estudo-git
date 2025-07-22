6a9ae55f4e23f82cf24821d25b8c2cdee2c2996a
Lucas Ribeiro <ribeirolucas962@gmail.com>
2025-07-21 22:25:04 -0300

    Verificando_par_ou _impar.py

--- menssagem.py
+++ menssagem.py
@@ -3,6 +3,12 @@ print(messagem)
 
 def soma( a, b ):
     return a + b
+def par_ou_impar(numero):
+    if numero % 2 == 0:
+        return "par"
+    else:
+        return "ímpar"
+    
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
@@ -10,3 +16,7 @@ resultado = soma(num1, num2)
 
 print(f"A soma de {num1} e {num2} é: {resultado}")
 
+if resultado.is_integer():
+    print (f"O núnmero {int(resultado)} é {par_ou_impar(int(resultado))}.")
+else:
+    print("o resultado não é um número inteiro. não é possível determinar se é par ou ímpar.")
